4673b360a441d41d998b8041658b5f6433441b34
omarrris <omarr.sheikh@gmail.com>
2024-07-04 15:53:52 -0400

    client

--- server/server.js
+++ server/server.js
@@ -6,7 +6,7 @@ const connectDB = require('./config/db');
 dotenv.config();
 
 const app = express();
-const PORT = process.env.PORT || 5000;
+const PORT = process.env.PORT || 5001;
 
 connectDB();
 
